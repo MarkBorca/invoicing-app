dad7b75d5d460cea0eea6f99c1d3a49752ffa346
borca mark <borcamark45@hotmail.com>
2022-11-21 20:09:18 +0100

    configuration
    base configuration

---
+++ client/src/components/styles/Global.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+    body {
+        background-color: ;
+        margin: 0;
+    }
+`
+
+export default GlobalStyles;
