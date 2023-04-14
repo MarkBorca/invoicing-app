57c6aab47d0f7a09a9c36d8bd45bdb8ef250a944
borca mark <borcamark45@hotmail.com>
2023-04-14 21:18:00 +0200

    Components inside the secondary navigation
    Added components for the different functionalities that the end user will be able to perform.

--- invoicing-app/Components/nav/NavbarLeftLarge.tsx
+++ invoicing-app/Components/nav/NavbarLeftLarge.tsx
@@ -1,8 +1,16 @@
+import LargeNavWidget from './large-nav-components/widget'
 
-export default function NavbarLeftLarge({navState}) {
-    return(
-        <div className={` bg-red-400 w-80 h-screen absolute top-0 left-0 z-20 ${navState ? 'block' : 'hidden'}`}>
-            <div></div>
-        </div>
-    )
+export default function NavbarLeftLarge({ navState }) {
+    return (
+            <div className={` bg-red-400 w-28 h-screen absolute top-0 left-0 z-20 ${navState ? "block" : "hidden"}`}>
+                <div className=" w-5/6 h-[385px] mt-16 mx-auto">
+                    <LargeNavWidget name='Home'/>
+                    <LargeNavWidget name='Invoices'/>
+                    <LargeNavWidget name='Quotations'/>
+                    <LargeNavWidget name='Clients'/>
+                    <LargeNavWidget name='Emails'/>
+                </div>
+            </div>
+        
+    );
 }
